61c445693578debdbc5f31b39816880085382be9
maxfire2008 <22722054+maxfire2008@users.noreply.github.com>
2022-11-08 19:59:51 +1100

    added archive feature

--- race_editor/app.py
+++ race_editor/app.py
@@ -2,15 +2,16 @@ import time
 import flask
 import yaml
 import flask_sqlalchemy
-import flask_wtf
-import wtforms
-import wtforms.validators
 
 db = flask_sqlalchemy.SQLAlchemy()
 
 app = flask.Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@127.0.0.1:5431"
 db.init_app(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 
 class Competition(db.Model):
@@ -20,14 +21,17 @@ class Competition(db.Model):
     gender = db.Column(db.String)
     ystart = db.Column(db.Integer)
     start_time = db.Column(db.DateTime)
+    event_id = db.Column(db.Integer, db.ForeignKey(Event.id))
+    archived = db.Column(db.Boolean)
 
 
 class Result(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    competition_id = db.Column(db.Integer, db.ForeignKey(Competition.id))
+    competition_id = db.Column(db.Integer)#, db.ForeignKey(Competition.id))
     # competition = db.relationship("Competition", backref=db.backref("competition", uselist=False))
     student_id = db.Column(db.String)
     score = db.Column(db.String)
+    archived = db.Column(db.Boolean)
     __table_args__ = (
         db.UniqueConstraint(
             competition_id,
@@ -46,60 +50,20 @@ def index():
     return flask.render_template("index.html")
 
 
-class CreateCompetitionForm(flask_wtf.FlaskForm):
-    name = wtforms.StringField(
-        'name',
-        validators=[
-            wtforms.validators.DataRequired()
-        ]
-    )
-    scoring_type = wtforms.StringField(
-        'scoring_type',
-        validators=[
-            wtforms.validators.DataRequired(),
-            wtforms.validators.AnyOf(
-                [
-                    "shortest_time",
-                    "highest_score"
-                ]
-            )
-        ]
-    )
-    gender = wtforms.StringField(
-        'gender',
-        validators=[
-            wtforms.validators.AnyOf(
-                [
-                    "male",
-                    "female",
-                    "any"
-                ]
-            )
-        ]
-    )
-    ystart = wtforms.IntegerField(
-        'ystart'
-    )
-    start_time = wtforms.DateTimeField(
-        'start_time'
-    )
-
-
-@app.route("/create_competition/", methods=["GET"]):
+@app.route("/create_competition/")
 def create_competition():
     return flask.render_template(
-        "create_competition.html",
-
+        "create_competition.html"
     )
 
 
-@app.route("/api/create_competition/", methods=["POST"])
-def api_create_competition():
+@app.route("/form/create_competition/", methods=["POST"])
+def form_create_competition():
     return "501 Not Implemented", 501
 
 @app.route("/edit_competition/<competition_id>")
 def edit_competition(competition_id):
-    results = Result.query.filter_by(competition_id=competition_id).all()
+    results = Result.query.filter_by(competition_id=competition_id, archived=False).all()
     return flask.render_template(
         "edit_competition.html",
         competition_id=competition_id,
@@ -145,6 +109,7 @@ def api_add_result(competition_id):
 @app.route("/api/delete_result/<result_id>", methods=["DELETE"])
 def api_delete_result(result_id):
     # return "200 OK", 200
-    Result.query.filter_by(id=result_id).delete()
+    result = Result.query.filter_by(id=result_id).first()
+    result.archived = True
     db.session.commit()
     return "200 OK", 200
